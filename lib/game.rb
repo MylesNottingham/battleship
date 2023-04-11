b3510c2624f817144888001fa4f73dce0c6b86aa
Jesse Thomas <jgthomas3@gmail.com>
2023-04-11 16:08:12 -0600

    Refactor: Make Game testable, play mthd.

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,11 @@
 class Game
-  attr_reader :winner
+  attr_reader :computer_cruiser,
+              :computer_submarine,
+              :human_cruiser,
+              :human_submarine,
+              :computer_board,
+              :human_board,
+              :winner
 
   def initialize
     @computer_cruiser = Ship.new("Cruiser", 3)
@@ -9,11 +15,13 @@ class Game
     @computer_board = Board.new
     @human_board = Board.new
     @winner = nil
+  end
 
+  def play
     computer_place(@computer_cruiser)
     computer_place(@computer_submarine)
 
-    3.times {puts}
+    3.times { puts }
     sleep(1)
     puts "I have laid out my ships on the grid.\n"\
     "You now need to lay out your two ships.\n"\
@@ -28,11 +36,11 @@ class Game
   end
 
   def computer_place(ship)
-      sleep(1)
-      loop do
-        coordinates = ship.length == 3 ? three_cell_coordinates.sample : two_cell_coordinates.sample
-        break if @computer_board.place(ship, coordinates)
-      end
+    sleep(1)
+    loop do
+      coordinates = ship.length == 3 ? three_cell_coordinates.sample : two_cell_coordinates.sample
+      break if @computer_board.place(ship, coordinates)
+    end
 
     puts @computer_board.render(true)
   end
@@ -44,6 +52,7 @@ class Game
       print "Enter the squares for the #{ship.name} (#{ship.length} spaces): "
       coordinates = gets.chomp.split(" ")
       break if @human_board.place(ship, coordinates)
+
       puts "Those are invalid coordinates. Please try again."
     end
   end
@@ -57,13 +66,13 @@ class Game
   def three_cell_coordinates
     three_cell_coordinates = []
     ("A".."D").each do |letter|
-      ("1".."2").each do |number|
-        three_cell_coordinates << [("#{letter}#{number}"), ("#{letter}#{number.to_i+1}"), ("#{letter}#{number.to_i+2}")]
+      (1..2).each do |number|
+        three_cell_coordinates << ["#{letter}#{number}", "#{letter}#{number + 1}", "#{letter}#{number + 2}"]
       end
     end
-    ("1".."4").each do |number|
+    (1..4).each do |number|
       ("A".."B").each do |letter|
-        three_cell_coordinates << [("#{letter}#{number}"), ("#{letter.next}#{number}"), ("#{letter.next.next}#{number}")]
+        three_cell_coordinates << ["#{letter}#{number}", "#{letter.next}#{number}", "#{letter.next.next}#{number}"]
       end
     end
     three_cell_coordinates
@@ -72,13 +81,13 @@ class Game
   def two_cell_coordinates
     two_cell_coordinates = []
     ("A".."D").each do |letter|
-      ("1".."3").each do |number|
-        two_cell_coordinates << [("#{letter}#{number}"), ("#{letter}#{number.to_i+1}")]
+      (1..3).each do |number|
+        two_cell_coordinates << ["#{letter}#{number}", "#{letter}#{number + 1}"]
       end
     end
-    ("1".."4").each do |number|
+    (1..4).each do |number|
       ("A".."C").each do |letter|
-        two_cell_coordinates << [("#{letter}#{number}"), ("#{letter.next}#{number}")]
+        two_cell_coordinates << ["#{letter}#{number}", "#{letter.next}#{number}"]
       end
     end
     two_cell_coordinates
