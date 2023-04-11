fd4ef1aa3eed0acf7a2f7b26d7cf647db82d7699
Jesse Thomas <jgthomas3@gmail.com>
2023-04-11 16:06:56 -0600

    Test: Add 7 tests to game_spec.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,25 +9,106 @@ RSpec.describe Game do
     it "exists" do
       expect(@game).to be_a(Game)
     end
+
+    it "has four ships" do
+      expect(@game.computer_cruiser).to be_a(Ship)
+      expect(@game.computer_submarine).to be_a(Ship)
+      expect(@game.human_cruiser).to be_a(Ship)
+      expect(@game.human_submarine).to be_a(Ship)
+    end
+
+    it "has two boards" do
+      expect(@game.computer_board).to be_a(Board)
+      expect(@game.human_board).to be_a(Board)
+    end
+
+    it "instantiates with no winner" do
+      expect(@game.winner).to eq(nil)
+    end
+  end
+
+  describe "#play" do
+    # pending development of turn class
   end
 
   describe "#computer_place" do
-    it "automatically places first ship on computer board" do
-      # expect(@board.cells["A1"]).not_to eq(first_a1)
+    it "allows computer to place first ship on computer board" do
+      cells = @game.computer_board.cells.values
+
+      expect(cells.all? { |cell| cell.status == "." }).to eq(true)
+      @game.computer_place(@game.computer_cruiser)
+      expect(cells.count { |cell| cell.status == "S" }).to eq(3)
     end
 
-    it "automatically places second ship on computer board" do
-      # expect(@board.cells["A1"]).not_to eq(first_a1)
+    it "allows computer to place a second ship on computer board" do
+      cells = @game.computer_board.cells.values
+
+      expect(cells.all? { |cell| cell.status == "." }).to eq(true)
+      @game.computer_place(@game.computer_cruiser)
+      @game.computer_place(@game.computer_submarine)
+      expect(cells.count { |cell| cell.status == "S" }).to eq(5)
     end
   end
 
   describe "#human_place" do
-    it "allows human to place first ship on computer board" do
-      expect(@board.cells["A1"]).not_to eq(first_a1)
-    end
+    # "you're gonna have to trust us that it works for humans too"
+  end
 
-    it "allows human to place second ship on computer board" do
-      # expect(@board.cells["A1"]).not_to eq(first_a1)
+  describe "#three_cell_coordinates" do
+    it "generates an array of all possible three cell placements" do
+      expected = [
+        ["A1", "A2", "A3"],
+        ["A2", "A3", "A4"],
+        ["B1", "B2", "B3"],
+        ["B2", "B3", "B4"],
+        ["C1", "C2", "C3"],
+        ["C2", "C3", "C4"],
+        ["D1", "D2", "D3"],
+        ["D2", "D3", "D4"],
+        ["A1", "B1", "C1"],
+        ["B1", "C1", "D1"],
+        ["A2", "B2", "C2"],
+        ["B2", "C2", "D2"],
+        ["A3", "B3", "C3"],
+        ["B3", "C3", "D3"],
+        ["A4", "B4", "C4"],
+        ["B4", "C4", "D4"]
+      ]
+
+      expect(@game.three_cell_coordinates).to eq(expected)
+    end
+  end
+
+  describe "#two_cell_coordinates" do
+    it "generates an array of all possible two cell placements" do
+      expected = [
+        ["A1", "A2"],
+        ["A2", "A3"],
+        ["A3", "A4"],
+        ["B1", "B2"],
+        ["B2", "B3"],
+        ["B3", "B4"],
+        ["C1", "C2"],
+        ["C2", "C3"],
+        ["C3", "C4"],
+        ["D1", "D2"],
+        ["D2", "D3"],
+        ["D3", "D4"],
+        ["A1", "B1"],
+        ["B1", "C1"],
+        ["C1", "D1"],
+        ["A2", "B2"],
+        ["B2", "C2"],
+        ["C2", "D2"],
+        ["A3", "B3"],
+        ["B3", "C3"],
+        ["C3", "D3"],
+        ["A4", "B4"],
+        ["B4", "C4"],
+        ["C4", "D4"]
+      ]
+
+      expect(@game.two_cell_coordinates).to eq(expected)
     end
   end
 end
