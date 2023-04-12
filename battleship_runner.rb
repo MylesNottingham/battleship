6e57d4c2e2681708c59347dc9299ae0e37d8ee88
Myles Nottingham <MylesNottingham@gmail.com>
2023-04-11 18:21:54 -0600

    Refactor: Create intro loop for multiple plays.

--- battleship_runner.rb
+++ battleship_runner.rb
@@ -1,26 +1,30 @@
 require "./lib/game"
-require "./lib/turn"
 require "./lib/board"
 require "./lib/ship"
 require "./lib/cell"
 
-game = Game.new
-3.times { puts }
-
-puts "Welcome to BATTLESHIP\n\n"
-sleep(0.5)
-puts "Enter p to play. Enter q to quit.!
-------------------------------------------------------------------"
-
-input = nil
-valid_input = ["P", "Q"]
 loop do
-  input = gets.upcase.chomp
-  break if valid_input.include?(input)
+  3.times { puts }
 
-  puts "INVALID INPUT"
+  puts "Welcome to BATTLESHIP\n\n"
+  sleep(0.5)
+  puts "Enter p to play. Enter q to quit.!
+  ------------------------------------------------------------------"
+
+  input = nil
+  valid_input = ["P", "Q"]
+  loop do
+    input = gets.upcase.chomp
+    break if valid_input.include?(input)
+
+    puts "INVALID INPUT"
+  end
+
+  if input == "P"
+    game = Game.new
+    game.start
+    puts game.winner
+  else
+    exit
+  end
 end
-
-input == "P" ? game.start : exit
-
-puts game.winner
