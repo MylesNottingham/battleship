7d7f5ea125984ed629d9e20de8f3e66ee3eec609
Myles Nottingham <MylesNottingham@gmail.com>
2023-04-11 20:29:12 -0600

    Feat: Fill in the methods a bit.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,31 +1,76 @@
-class Turn 
-  attr_reader :winner
+class Turn
+  attr_reader :turn_winner
 
-  def initialize(computer_board, human_board)
+  def initialize(
+    computer_cruiser,
+    computer_submarine,
+    human_cruiser,
+    human_submarine,
+    computer_board,
+    human_board
+  )
+    @computer_cruiser = computer_cruiser
+    @computer_submarine = computer_submarine
+    @human_cruiser = human_cruiser
+    @human_submarine = human_submarine
     @computer_board = computer_board
     @human_board = human_board
     @turn_winner = nil
 
+    display_boards
+    human_shot
+    computer_shot
+    display_results
+    determine_winner
   end
 
   def display_boards
-    # will just call render for both boards
+    puts "=============COMPUTER BOARD============="
+    puts @computer_board.render
+
+    puts "==============PLAYER BOARD=============="
+    puts @human_board.render(true)
+    puts
   end
 
+  # def human_shot
+  #   input = gets.chomp
+  #   while @computer_board.cells[input].fired_upon?
+  #     puts "Already fired upon"
+  #     input = gets.chomp
+  #   end
+  #   @computer_board.cells[input].fire_upon
+  # end
+
   def human_shot
-    # prompt for coordinate with a loop that only allows for valid_coordinates
+    @human_shot = nil
+
+    puts "Enter the coordinate for your shot: "
+    loop do
+      @human_shot = gets.chomp
+      break if @computer_board.valid_coordinate?(@human_shot) && !@computer_board.cells[@human_shot].fired_upon?
+      puts "Please enter a valid coordinate: "
+    end
+
+    @computer_board.cells[@human_shot].fire_upon
   end
 
   def computer_shot
-    # automatically choose a valid coordinate
+    loop do
+      @computer_shot = @human_board.cells.keys.sample
+      break unless @human_board.cells[@computer_shot].fired_upon?
+    end
+    @human_board.cells[@computer_shot].fire_upon
   end
 
-  def results
-    # print results
+  def display_results
+    @computer_board.cells[@human_shot].status == "M" ? human_result = "miss" : human_result = "hit"
+    puts "Your shot on #{@human_shot} was a #{human_result}."
+    @human_board.cells[@computer_shot].status == "M" ? computer_result = "miss" : computer_result = "hit"
+    puts "My shot on #{@computer_shot} was a #{computer_result}."
   end
 
   def determine_winner
-    @winner = @turn_winner
+    @turn_winner = "TEST"
   end
-
 end
