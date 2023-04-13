7df07dc09307b29ee6d58fae1033f91e40710c4c
Jesse Thomas <jgthomas3@gmail.com>
2023-04-13 08:20:06 -0600

    Refactor: Add comments to Cell class.

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,19 +1,25 @@
+# Establishes Cell class
+
 class Cell
   attr_reader :coordinate,
               :ship,
               :status
 
+  # Instantiates with three attributes and one coordinate parameter
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
     @status = "."
   end
 
+  # Changes cell status to represent a ship
   def place_ship(ship)
     @ship = ship
     @status = "S"
   end
 
+  # Adjusts cell status based on current status when called
+  # Will return false if status can't be accessed
   def fire_upon
     if @status == "."
       @status = "M"
@@ -25,6 +31,8 @@ class Cell
     end
   end
 
+  # Will reveal Cell status if passed true
+  # Cell status dependent on current status at the time
   def render(reveal = false)
     if @status == "S"
       reveal ? "S" : "."
@@ -35,10 +43,12 @@ class Cell
     end
   end
 
+  # Returns true if status is "empty"
   def empty?
     @status == "."
   end
 
+  # Returns true if status is already a miss or hit
   def fired_upon?
     @status == "M" || @status == "H"
   end
