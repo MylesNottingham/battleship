89e85693cf2a28b5ebe9541346ac68ee3584635f
Jesse Thomas <jgthomas3@gmail.com>
2023-04-13 10:02:41 -0600

    Refactor: Add Ship comments.

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,9 +1,11 @@
+# Establishes Ship class
 class Ship
   attr_reader :name,
               :length,
               :health,
               :sunk
 
+  # Instantiates with four variables
   def initialize(name, length)
     @name = name
     @length = length
@@ -11,10 +13,12 @@ class Ship
     @sunk = false
   end
 
+  # Returns sunk status
   def sunk?
     @sunk
   end
 
+  # Takes health from ship and establishes sunk status
   def hit
     @health -= 1 unless @health <= 0
     @sunk = true if @health.zero?
