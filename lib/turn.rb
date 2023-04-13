4759a8ace02d60c96ef7b9ffac46b4d8e5d223dc
Jesse Thomas <jgthomas3@gmail.com>
2023-04-13 09:50:37 -0600

    Refactor: Add comments to Turn class.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,8 @@
+# Establishes Turn class
 class Turn
   attr_reader :turn_winner
 
+  # Instantiates with instance objects from Game class
   def initialize(
     computer_cruiser,
     computer_submarine,
@@ -18,6 +20,7 @@ class Turn
     @turn_winner = nil
   end
 
+  # Calls methods responsible for game play in order
   def start
     display_boards
     human_shot
@@ -26,6 +29,7 @@ class Turn
     determine_winner
   end
 
+  # Displays empty boards and will show user ship positions
   def display_boards
     puts  "=============COMPUTER BOARD=============\n"\
           "#{@computer_board.render}"\
@@ -33,6 +37,7 @@ class Turn
           "#{@human_board.render(true)}"
   end
 
+  # Takes user shot after getting valid input
   def human_shot
     @human_shot = nil
 
@@ -44,9 +49,11 @@ class Turn
       print "Please enter a valid coordinate: "
     end
 
+    # Fires on computer board if satisfied
     @computer_board.cells[@human_shot].fire_upon
   end
 
+  # Randomizes computer shot once shot is validated
   def computer_shot
     loop do
       @computer_shot = @human_board.cells.keys.sample
@@ -55,6 +62,7 @@ class Turn
     @human_board.cells[@computer_shot].fire_upon
   end
 
+  # Displays both players' boards with new shot results
   def display_results
     sleep(1)
     human_result = @computer_board.cells[@human_shot].status == "M" ? "miss" : "hit"
@@ -65,6 +73,7 @@ class Turn
     sleep(1)
   end
 
+  # Determines winner if both ships are returning as sunk 
   def determine_winner
     if @computer_cruiser.sunk? && @computer_submarine.sunk? && @human_cruiser.sunk? && @human_submarine.sunk?
       display_boards
